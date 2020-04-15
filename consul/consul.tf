b49de08584150e99d460ccd5232532c3d8139c12
Emil Engfors <engfors.emil@gmail.com>
2020-04-15 17:20:37 +0200

    Update consul.tf

--- consul/consul.tf
+++ consul/consul.tf
@@ -2,7 +2,7 @@ resource "helm_release" "consul" {
   count      = data.terraform_remote_state.cluster.outputs.enable_consul_and_vault ? 1 : 0
   depends_on = [kubernetes_namespace.secrets]
   name       = "${var.release_name}-consul"
-  chart      = "${path.module}/consul-helm"
+  chart      = "/consul-helm"
   namespace  = var.namespace
 
   set {
