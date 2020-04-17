59b41dae932d60aee16b696de9f9934b4a6af2de
Emil Engfors <engfors.emil@gmail.com>
2020-04-17 16:22:39 +0200

    adding required APIs

--- cluster/main.tf
+++ cluster/main.tf
@@ -13,3 +13,17 @@ provider "google" {
   project = var.google_project
   region  = var.location.gcp
 }
+
+# Enables Compute Engine API
+resource "google_project_service" "compute" {
+  project            = google_project.ch-pds-test.project_id
+  service            = "compute.googleapis.com"
+  disable_on_destroy = false
+}
+
+# Enables Kubernetes Engine API
+resource "google_project_service" "container" {
+  project            = google_project.ch-pds-test.project_id
+  service            = "container.googleapis.com"
+  disable_on_destroy = false
+}
